26142416cf88a0958f4493bd36b0b5969684edd4
Lachlan Donald <lachlan@ljd.cc>
2024-12-01 07:14:51 +1100

    Use hermit in buildkite pipeline

--- .buildkite/steps/tests.sh
+++ .buildkite/steps/tests.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-go version
-echo arch is "$(uname -m)"
-
-go install gotest.tools/gotestsum@v1.8.0
-
 # Install pact-go
 go install github.com/pact-foundation/pact-go/v2@latest
 pact-go -l DEBUG install
